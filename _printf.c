f16e8fa09b22ae0bf53917609099eaf1589590b6
pablox85 <pdpcorrales@gmail.com>
2025-03-27 20:42:01 +0000

    update

--- _printf.c
+++ _printf.c
@@ -19,17 +19,17 @@ int _printf(const char *format, ...)
 		if (format[i] == '%')
 		{
 			i++;
-			if (format[i] == 'c')
-				count += print_char(args);
-			else if (format[i] == '%')
-				count += write(1, "%", 1);
-			else if (format[i] == 's')
-				count += print_string(args);
-			else if (format[i] == 'd' || format[i] == 'i')
-				count += print_int(args);
-			else {
-				count += write(1, "%", 1);
-				count += write(1, &format[i], 1);
+			switch (format[i])
+			{
+				case 'c': count += print_char(args); break;
+				case '%': count += write(1, "%", 1); break;
+				case 's': count += print_string(args); break;
+				case 'd': 
+				case 'i': count += print_int(args); break;
+				default: 
+					count += write(1, "%", 1);
+					count += write(1, &format[i], 1);
+					break;
 			}
 		}
 		else
